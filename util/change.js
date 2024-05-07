bcf882392505a614803c30e8bfbaafb7874b99a8
Danil <danil.shevelev.2004@mail.ru>
2024-05-07 22:51:19 +0500

    change command

---
+++ util/change.js
@@ -0,0 +1,12 @@
+import { readFileSync, writeFileSync } from "node:fs";
+
+import { WEEKS_FILE_PATH, CUR_WEEK_FILE_PATH } from "./common.js";
+
+export function change(numOfDay, numOfClass, newText) {
+    let curWeek = readFileSync(CUR_WEEK_FILE_PATH);
+    let weeks = JSON.parse(readFileSync(WEEKS_FILE_PATH));
+    
+    weeks[curWeek][numOfDay - 1][numOfClass - 1] = newText;
+
+    writeFileSync(WEEKS_FILE_PATH, JSON.stringify(weeks, null, 4));
+}
